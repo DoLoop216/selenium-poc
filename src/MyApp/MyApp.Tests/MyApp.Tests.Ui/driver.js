334e7d0b8706e79e9d5c72ae5779596f02627599
Aleksa Ristic <aristiccitsira@gmail.com>
2024-08-14 01:46:10 +0200

    Dockerized selenium tests

--- src/MyApp/MyApp.Tests/MyApp.Tests.Ui/driver.js
+++ src/MyApp/MyApp.Tests/MyApp.Tests.Ui/driver.js
@@ -1,9 +1,43 @@
-import webdriver, {Capabilities} from "selenium-webdriver";
+import webdriver, {Builder, Capabilities} from "selenium-webdriver";
 import Firefox from "selenium-webdriver/firefox.js";
+import Chrome from "selenium-webdriver/chrome.js";
 
 export const createDriver = () => {
-    return new webdriver.Builder()
-        .withCapabilities(Capabilities.firefox().set("acceptInsecureCerts", true))
-        .setFirefoxOptions(new Firefox.Options())
-        .build()
+    if (process.env.ENV === 'local') {
+        return createLocalDriver()
+    }
+    else if (process.env.ENV === 'github-action') {
+        return createRemoteDriver()
+    }
+    else {
+        throw new Error('Unsupported environment')
+    }
+}
+
+const createLocalDriver = () => {
+    if (process.env.BROWSER === 'firefox') {
+        let options = new Firefox.Options()
+        return new webdriver.Builder()
+            .withCapabilities(Capabilities.firefox().set("acceptInsecureCerts", true))
+            .setFirefoxOptions(options)
+            .build()
+    }
+    else if (process.env.BROWSER === 'chrome') {
+        let options = new Chrome.Options()
+        return new webdriver.Builder()
+            .withCapabilities(Capabilities.chrome().set("acceptInsecureCerts", true))
+            .setChromeOptions(options)
+            .build()
+    }
+    else {
+        throw new Error('Unsupported browser')
+    }
+}
+
+const createRemoteDriver = () => {
+    const seleniumServer = process.env.SELENIUM_SERVER || 'selenium'
+    return new Builder()
+        .usingServer(`http://${seleniumServer}:4444`)
+        .forBrowser(process.env.BROWSER)
+        .build();
 }
